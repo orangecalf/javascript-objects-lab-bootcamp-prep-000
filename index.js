a46de6dbee7c9a8fa7c51a03ba45176afc464789
yossi katzman <issoyk@gmail.com>
2018-02-22 16:11:02 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ function deleteFromObjectByKey(object, key) {
   
 }
 
-function destructivdeleteFromObjectByKey(object, key) {
+function destructivelyDeleteFromObjectByKey(object, key) {
   delete object[key]
   return object
   
